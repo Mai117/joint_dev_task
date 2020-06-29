ce07f7d9e1da2c78580549fbd0d3f3d9607bc678
mai Matsuda <mai117@mai-no-MacBook-Pro.local>
2020-06-29 22:51:20 +0900

    Q4,7,10,11,12,15修正しました

--- task.rb
+++ task.rb
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -54,8 +54,10 @@ end
 def q7
   array = ["1", "2", "3", "4", "5"]
 
-  # 以下に回答を記載
-  array.map!{|x| x.to_i}
+  # 以下に回答を記載 
+  array.map!(&:to_i)
+  # array.map!{|x| x.to_i} を&:で簡潔に
+
   # 以下は変更しないで下さい
   p array
 end
@@ -84,29 +86,33 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
-  end
+  puts foods.include?("うに") ? "好物です" : "まぁまぁ好きです"
 end
+  # if foods.include?("うに")
+  #   puts "好物です"
+  # else
+  #   puts "まぁまぁ好きです"
+  # end  を三項演算子で簡潔に
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!
-  sports = sports.uniq
-  sports.each.with_index(1) do |sport, i|
+  sports.flatten!.uniq.each.with_index(1) do |sport, i|
     puts "No#{i} #{sport}"
   end
 end
+  # sports.flatten!
+  # sports = sports.uniq
+  # sports.each.with_index(1) do |sport, i|
+  #   puts "No#{i} #{sport}"
+  # end は 全てsportsに対してメソッドだから.で繋げることができる
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-  puts data.slice(:user, :name)
+  puts data[:user][:name]
 end
 
 def q13
@@ -130,17 +136,19 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.include?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-  if data2.include?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1.key?(:age) ? "OK" : "NG"
+  puts data2.key?(:age) ? "OK" : "NG"
 end
+  # if data1.key?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end
+  # if data2.key?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end  を三項演算子で簡潔に
 
 def q16
   users = [
@@ -149,9 +157,10 @@ def q16
     { name: "takahashi", age: 32 },
     { name: "nakamura", age: 41 }
   ]
-
   # 以下に回答を記載
-  
+  users.each_value do |name, age|
+    puts "私の名前は#{name}です。年齢は#{age}歳です。"
+  end
 end
 
 class UserQ17
